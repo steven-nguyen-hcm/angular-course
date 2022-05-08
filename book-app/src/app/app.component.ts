601a2521bff4bc1a6e76ea8dfcd6b26327c98145
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-05-09 01:59:19 +0700

    lecture 86

--- book-app/src/app/app.component.ts
+++ book-app/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+enum Feature {
+  SHOPPING = 'shopping',
+  RECIPE = 'recipe'
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedFeature: Feature = Feature.RECIPE; 
+  feature: {} = Feature;
+
+  onNavigate(feature: Feature) {
+    console.log(feature);  
+    this. selectedFeature= feature;
+  }
 }
--- book-app/src/app/header/header.component.ts
+++ book-app/src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output()
+  featureSelected: EventEmitter<string> = new EventEmitter();
 
+  onSelected(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
