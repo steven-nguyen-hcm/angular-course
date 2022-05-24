c231709c25df4d962cae232dcccde270a9ea3202
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-05-25 00:27:37 +0700

    finish lecture 155

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
 import { ErrorPageComponent } from "./error-page/error-page.component";
 import { HomeComponent } from "./home/home.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerResolver } from "./servers/server/server-resolver.service";
