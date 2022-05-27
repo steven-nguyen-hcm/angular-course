c513403c137d350c0b5f7d4cd2601850ec6465e9
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-05-27 22:31:06 +0700

    Finished lecture 182

--- prj-routing-final/src/app/shopping-list/shopping-list.component.ts
+++ prj-routing-final/src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "./shopping-list.service";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private ingredientsChangedSubscription: Subscription;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
-      .subscribe(
+    this.ingredientsChangedSubscription =
+      this.slService.ingredientsChanged.subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       );
+  }
+
+  ngOnDestroy() {
+    this.ingredientsChangedSubscription.unsubscribe();
   }
 }
