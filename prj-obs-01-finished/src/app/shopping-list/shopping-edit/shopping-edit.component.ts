17eefc23360d2a7f5c3c6aaa0fd83dd167f46559
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-06-04 00:51:12 +0700

    Finished lecture 219

---
+++ prj-obs-01-finished/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Ingredient } from "../../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list.service";
+
+
+@Component({
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"],
+})
+export class ShoppingEditComponent implements OnInit {
+
+  constructor(private slService: ShoppingListService) {}
+
+  ngOnInit() {}
+
+  onAddItem(form: NgForm) {
+    const formValue = form.value;
+    const ingName = formValue.name;
+    const ingAmount = formValue.amount;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
+  }
+}
