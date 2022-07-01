8ef152576792b1a0be73dee92b48e81b46db122e
Steven Nguyen <steven.nguyen@contemi.com>
2022-07-02 01:24:32 +0700

    354

---
+++ standalone-component-with-routing/src/app/dashboard/today/today.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  templateUrl: './today.component.html',
+  standalone: true,
+})
+export class TodayComponent {}
